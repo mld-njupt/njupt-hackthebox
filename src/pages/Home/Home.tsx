811158f80202740558cca113259a1eeacc933c97
lili kai <1761311608@qq.com>
2022-02-07 20:39:54 +0800

    complete demo init

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { useState, useCallback } from "react";
-import { Layout, Menu, Message } from "@arco-design/web-react";
-import { Routes, Link, Route, Outlet } from "react-router-dom";
+import { Layout, Menu } from "@arco-design/web-react";
+import { useNavigate, Link, Outlet } from "react-router-dom";
 import { lazy } from "react";
 import IconAbout from "../../assets/images/icons/About.svg";
 import IconDashboard from "../../assets/images/icons/Dashboard.svg";
@@ -9,16 +9,19 @@ import IconEnv from "../../assets/images/icons/Env.svg";
 import IconOnline from "../../assets/images/icons/Online.svg";
 import "./Home.scss";
 const MenuItem = Menu.Item;
-const SubMenu = Menu.SubMenu;
 const Sider = Layout.Sider;
 const Header = Layout.Header;
-const Footer = Layout.Footer;
 const Content = Layout.Content;
 const Home = () => {
   // const [collapsed, setCollapsed] = useState(false);
   // const handleCollapsed = useCallback(() => {
   //   setCollapsed(!collapsed);
   // }, [collapsed]);
+  const navigate = useNavigate();
+  const handleCilckMenuItem = (key) => {
+    navigate(key);
+    console.log(key);
+  };
   return (
     <div className="home-wrap">
       <Layout
@@ -33,40 +36,44 @@ const Home = () => {
         </Header>
         <Layout>
           <Sider breakpoint="xl">
-            <Menu defaultSelectedKeys={["Dashboard"]} style={{ width: "100%" }}>
+            <Menu
+              defaultSelectedKeys={["Dashboard"]}
+              style={{ width: "100%" }}
+              onClickMenuItem={handleCilckMenuItem}
+            >
               <MenuItem
-                key="Dashboard"
+                key="/dashboard"
                 style={{ display: "flex", alignItems: "center" }}
               >
                 <IconDashboard
                   className="arco-icon"
-                  style={{ fontSize: "24px" }}
+                  style={{ fontSize: "24px", color: "#9fef00" }}
                 />
-                <Link to="/dashboard">首页/Dashboard</Link>
+                首页/Dashboard
               </MenuItem>
               <MenuItem
-                key="Env"
+                key="/env"
                 style={{ display: "flex", alignItems: "center" }}
               >
                 <IconEnv className="arco-icon" style={{ fontSize: "24px" }} />
-                <Link to="/env">实操环境/Env</Link>
+                实操环境/Env
               </MenuItem>
               <MenuItem
-                key="Online"
+                key="/online"
                 style={{ display: "flex", alignItems: "center" }}
               >
                 <IconOnline
                   className="arco-icon"
                   style={{ fontSize: "24px" }}
                 />
-                <Link to="/online">线上对战/Online</Link>
+                线上对战/Online
               </MenuItem>
               <MenuItem
-                key="About"
+                key="/about"
                 style={{ display: "flex", alignItems: "center" }}
               >
                 <IconAbout className="arco-icon" style={{ fontSize: "24px" }} />
-                <Link to="/about">运营/About</Link>
+                运营/About
               </MenuItem>
             </Menu>
           </Sider>
